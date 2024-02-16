0b878c2026f213cdc744d53249cc72b36be85be9
ZachMK25 <k.zach@wustl.edu>
2024-02-16 12:45:35 -0600

    deleted auth function files, functions need to be implemented in firebaseConfig.js

--- SpotifyGames/auth_signin_password.js
+++
@@ -1,6 +0,0 @@
-/**
- * Create a form that allows existing users to sign in using their email address and password. 
- * When a user completes the form, call the signInWithEmailAndPassword method:
- */
-
-
