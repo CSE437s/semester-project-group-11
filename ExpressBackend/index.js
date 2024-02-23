64a6db12b34f2ad8333e3d4cddfef8b4457043c4
ZachMK25 <k.zach@wustl.edu>
2024-02-22 18:18:47 -0600

    updated backend for use with registration

--- ExpressBackend/index.js
+++ ExpressBackend/index.js
@@ -1,21 +1,26 @@
 const express = require('express');
-const {initializeApp} = require('firebase-admin/app');
+// const {initializeApp} = require('firebase-admin/app');
 const admin = require('firebase-admin');
-const {getFirestore} = require('firebase/firestore');
 const asyncHandler = require('express-async-handler');
 const cors = require('cors');
-const ngrok = require('ngrok');
+const fs = require('fs');
+const dotenv = require('dotenv')
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-initializeApp({
+
+// GET IP THROUGH TERMINAL COMMAND FIRST, STORE IN ENV?
+
+const firebaseApp = admin.initializeApp({
   credential: admin.credential.applicationDefault(),
   databaseURL: "https://group11-8368c-default-rtdb.firebaseio.com"
 });
 
-const db = admin.database();
+const db = admin.firestore();
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -23,68 +28,154 @@ app.use((req, res, next) => {
   next();
 });
 
-async function isUniqueUsername(username) {
+async function addUser(username, email) {
   try {
-      const usersRef = collection(db, 'users');
-      const q = query(usersRef, where('username', '==', username));
-      const querySnapshot = await getDocs(q);
-      return querySnapshot.empty
+      const ref = doc(db, "users", username).withConverter(userConverter);
+      await setDoc(ref, new User(username, email))
+      return true;
+  } catch (e) {
+      console.error("Error adding document: ", e);
+      return false;
+  }
+}
+
+async function isUniqueEmail(email) {
+  try {
+    const usersRef = db.collection('users');
+    const allUsers = await usersRef.orderBy("username").get();
+    allUsers.forEach((doc) => {
+      console.log(doc.id, '=>', doc.data());
+    });
+    const snapshot = await usersRef.where('email', '==', email).get();
+    return snapshot.empty
   }
   catch (error) {
-      
+      throw Error("error querying usernames")
+  }
+}
+
+async function isUniqueUsername(username) {
+  try {
+    const usersRef = db.collection('users');
+    const snapshot = await usersRef.where('username', '==', username).get();
+    return snapshot.empty
+  }
+  catch (error) {
       throw Error("error querying usernames")
   }
 }
 
 function isUsernameSanitized(username) {
-  return /^[0-9a-zA-Z_.-]+$/.test(username);
+
+  return /^[0-9a-z_.-]+$/.test(username);
 }
 
-app.post("/user/validate", asyncHandler(async (req, res) => {
+app.post("/user/add", asyncHandler(async (req, res) => {
 
-  console.log("got it")
-
-  const username = req.body.data.username;
-
-  if (!isUsernameSanitized(username)){
-    return res.status(400).json({isUniqueUsername:false, message:"Username not sanitized"});
-  }
-
+  const username = String(req.body.data.username).toLowerCase();
+  const email = req.body.data.email;
+  
   try{
-    const isUnique = await isUniqueUsername(username);
-    if (isUnique){
-      return res.status(200).json({message: "Success", isUnique:true});
+
+    const sanitizedUsername = isUsernameSanitized(username);
+
+    if (!sanitizedUsername){
+      return res.status(400).json({message: "Username not sanitized"});
     }
-    else{
-      return res.status(400).json({message: "Username already exists", isUnique:false});
+    console.log("username sanitized...");
+    const uniqueEmail = await isUniqueEmail(email);
+
+    if (!uniqueEmail){
+      return res.status(400).json({message: "Email is already in use"});
     }
+
+    console.log("email is unique...");
+
+
+    const uniqueUsername = await isUniqueUsername(username);
+    if (!uniqueUsername){
+      return res.status(400).json({message: "Username already exists"});
+    }
+
+    console.log("username is unique...");
+
+    const addedUser = await addUser(username, email);
+
+    if (!addedUser){
+      return res.status(500).json({message:"unable to add user to database"})
+    }
+
+    console.log("user is added!");
+
+
+    return res.status(200).json({message: "Success"});
   }
   catch (error) {
-    return res.status(500).json({message: "Error querying username in database"});
+    // console.log(error)
+    return res.status(500).json({message: "Error registering user", error:error});
   }
 }));
 
-const PORT = 16969;
+// app.post("/user/username/sanitized", asyncHandler(async (req, res) => {
+//   if (!req.body.data.username){
+//     console.log("no username");
+//     return res.status(400).json({sanitized:false, username: null, message:"Username not given"});
+//   }
+//   const username = new String(req.body.data.username);
+//   const usernameLower = username.toLowerCase();
+//   console.log("username??", usernameLower);
+//   if (isUsernameSanitized(usernameLower)){
+//     return res.status(200).json({sanitized:true, username:usernameLower, message:"Username is sanitized"});
+//   }
+//   else{
+//     return res.status(400).json({sanitized:false, username: null, message:"Username not sanitized"});
+//   }
 
-// Use ngrok to create a tunnel to your local server
-const startNgrok = async () => {
-  try {
-    const url = await ngrok.connect(PORT);
-    console.log(`Ngrok tunnel is active at ${url}`);
-  } catch (err) {
-    console.error('Error starting ngrok:', err);
-  }
-};
+// }))
 
-// Start the Express server and ngrok tunnel
-const startServer = async () => {
-  await app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+// app.post("/user/username/unique", asyncHandler(async (req, res) => {
 
-  // Start ngrok after the server has started
-  await startNgrok();
-};
+//   const username = req.body.data.username;
+  
+//   try{
+//     const isUnique = await isUniqueUsername(username);
+//     if (isUnique){
+//       return res.status(200).json({message: "Success", unique:true});
+//     }
+//     else{
+//       return res.status(400).json({message: "Username already exists", unique:false});
+//     }
+//   }
+//   catch (error) {
+//     console.log(error)
+//     return res.status(500).json({message: "Error querying username in database"});
+//   }
+// }));
 
-// Start the server
-startServer();
+// app.post("/user/email/unique", asyncHandler(async (req, res) => {
+
+//   const email = req.body.data.email;
+  
+//   try{
+//     const isUnique = await isUniqueEmail(email);
+//     if (isUnique){
+//       return res.status(200).json({message: "Success", unique:true});
+//     }
+//     else{
+//       return res.status(400).json({message: "Email already exists", unique:false});
+//     }
+//   }
+//   catch (error) {
+//     console.log(error)
+//     return res.status(500).json({message: "Error querying username in database"});
+//   }
+// }));
+
+app.get('/test', (req, res) => {
+  console.log("got it")
+  res.status(200).json({ message: 'This is a test endpoint.' });
+});
+
+const PORT = 16969 || process.env.PORT;
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
--- README.md
+++ README.md
@@ -10,6 +10,22 @@
 
 ## How to Run:
 
+First, start off by getting the IP address of the computer that will host the server:
+
+Open a terminal and run the following command based on your OS
+MacOS:
+```
+ipconfig getifaddr en0
+```
+
+Windows:
+```
+ipconfig | findstr /C:"IPv4"
+```
+
+Next, take the IP address and paste it into SpotifyGames/.env in the section for EXPO_PUBLIC_SERVER_IP
+
+
 ```
 cd SpotifyGames
 npx expo start
